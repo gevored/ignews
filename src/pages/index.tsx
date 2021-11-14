52d591afbb70b202082166643a49cea912740919
gevored <gersonrevoredo@hotmail.com>
2021-11-14 14:07:16 -0300

    menu done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
-import styles from '../styles/home.module.scss'
+
+import Head from 'next/head' 
 
 export default function Home() {
   return (
-    <h1 className = {styles.title}>
-      Hello
-    </h1>
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+
+      <h1>
+
+        Oi
+      </h1>
+    </>
   )
 }
